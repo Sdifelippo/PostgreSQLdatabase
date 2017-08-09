42eb94e4d1047d21d1519792f758843193f06003
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-08 20:48:56 -0400

    showingtablenotupdatingordeleting

---
+++ todo.sql
@@ -0,0 +1,74 @@
+--drops table creates table each run
+DROP TABLE if EXISTS todos;
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  details text,
+  priority  INTEGER NOT NULL DEFAULT 1,
+  created_at TIMESTAMP NOT NULL,
+  completed_at  TIMESTAMP
+);
+
+INSERT INTO todos (
+            title,
+            details,
+            priority,
+            created_at,
+            completed_at
+          );
+
+VALUES (
+            'wash car',
+            'mix some soap and water and wash car',
+            3,
+            '8/8/2017',
+            NULL
+          ),
+          (
+            'clean my underoos',
+            'mix some soap and water and wash underoos',
+            1,
+            '8/8/2017',
+            NULL
+          ),
+          (
+            'walk the kronos monster',
+            'walk my dog around the neighborhood',
+            2,
+            NULL,
+            NULL
+          ),
+          (
+            'take out the trash',
+            'take the trash out and place in trash bin',
+          4,
+            NULL,
+            '8/8/2017'
+          ),
+          (
+            'play Destiny 2',
+            'whoop some dudes in the crucible and maybe complete a raid',
+            5,
+            NULL,
+            NULL
+          );
+
+--show entire table
+SELECT * FROM todos;
+
+--write select statement to find all incomplete todos
+SELECT title, completed_at
+FROM todos
+WHERE completed_at IS NULL;
+
+-- Write a SELECT statement to find all todos with a priority above 1
+SELECT title, priority
+FROM todos
+WHERE priority > 1;
+-- Write an UPDATE statement to complete one todo by its id.
+UPDATE todos
+SET completed_at = now()
+WHERE id = 3;
+-- Write a DELETE statement to delete all completed todos.
+DELETE FROM todos
+WHERE completed_at IS NOT NULL;
